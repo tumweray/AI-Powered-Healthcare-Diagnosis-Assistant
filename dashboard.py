71e80f00afd97d66261797941b40e1d436cf970b
Raymond <tumweray@gmail.com>
2024-01-13 11:04:28 +0300

    random forest and dashboard files

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+"""
+ Data presentation
+ 1. In app - creation of a dashboard where visualisation 
+ and presentation is done within an app
+ 2. Off app - here desktop applications like spyder,jupter 
+ and colab notebooks and other editors are used to do data
+   processing and visualisation .
+ 3.shortcoming for off app is that there is limited accessibility of 
+ data to enable decision making,
+   there is need to learn in app.
+
+4 Technologies needed:
+  Python(HLL)
+  Django and flask (frameworks)
+"""
