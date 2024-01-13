d4c76318e0c7e437a889c4e5980fad275e8de6d6
Raymond <tumweray@gmail.com>
2024-01-13 12:13:32 +0300

    more content via dashboard,

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,36 @@
  data to enable decision making,
    there is need to learn in app.
 
-4 Technologies needed:
-  Python(HLL)
-  Django and flask (frameworks)
+4 Technologies needed for in app :
+    -Python(HLL)
+    -Django and flask (frameworks)
+    -html(hypertext markup langauge) and css(casacading style sheets)
+    -javascript
+
+Web dev workson a 3 tier architecture.
+1. frontend (client side) - sends a request to webserver and interprets a response.
+e.g - web browsers
+
+2. Web server/backend - computer with server software connected to the internet.
+- receives requests from the client. 
+(-where html and css is stored
+ -webpages are stored) 
+ e.g - Apache
+     - MSIS
+     - NGINX
+     - Tomcat
+website is a collection of related web pages
+webhousting - transfering webpages to a webserver we can it can be accessed remotely.
+localhost - a computer acts as both a client and webserver e.g laptop.
+
+3 .Database server/dbms(data is stored as plain text or tables, 
+picture are stored in a binary format called blob.
+database can be a relational or a document based database.) 
+e.g -MariaDb or MySQL(open-source) relational DB , 
+    -MS SQL(commercial)relational DB,
+    -Postgres(GIS suitable)relational DB,
+    -SQL lite (relational DB,file based DB,used in phones),
+    -MongoDB(document rated,tabulated)
+    -Oracle(Disturbuted systems like banks)
+
 """
